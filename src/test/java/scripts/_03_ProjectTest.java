6d3856584a18a281ec977d7d794ec53c440c8308
cihan avsar degertas <cavshar21@gmail.com>
2023-06-09 23:42:38 -0600

    updated Project3

--- src/test/java/scripts/_03_ProjectTest.java
+++ src/test/java/scripts/_03_ProjectTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.DropdownHandler;
 
 import java.util.List;
 
@@ -32,36 +33,59 @@ public void validateBookYourTrip(){
     WebElement oneWayRadioButton = driver.findElement(By.cssSelector("input[value='One way']"));
     Assert.assertTrue(oneWayRadioButton.isDisplayed());
     Assert.assertTrue(oneWayRadioButton.isSelected());
+
     WebElement roundTripRadioButton = driver.findElement(By.cssSelector("input[value='Round trip']"));
     Assert.assertTrue(roundTripRadioButton.isDisplayed());
     Assert.assertFalse(roundTripRadioButton.isSelected());
+
     WebElement cabinClassLAbel =driver.findElement(By.cssSelector(".field>div:nth-child(2)"));
     Assert.assertTrue(cabinClassLAbel.isDisplayed());
     WebElement cabinClassDropdown= driver.findElement(By.cssSelector(".field>div:nth-child(2)>select:nth-child(1)"));
     Assert.assertTrue(cabinClassDropdown.isDisplayed());
+
     WebElement fromLAbel =driver.findElement(By.cssSelector("form>div>div:nth-child(3)>label"));
     Assert.assertTrue(fromLAbel.isDisplayed());
     WebElement fromDropdown= driver.findElement(By.cssSelector("form>div>div:nth-child(3)>div"));
     Assert.assertTrue(fromDropdown.isDisplayed());
+
     WebElement toLAbel =driver.findElement(By.cssSelector("form>div>div:nth-child(4)>label"));
     Assert.assertTrue(toLAbel.isDisplayed());
     WebElement toDropdown= driver.findElement(By.cssSelector("form>div>div:nth-child(4)>div"));
     Assert.assertTrue(toDropdown.isDisplayed());
+
     WebElement departLabel = driver.findElement(By.cssSelector("form>div>div:nth-child(5)>label"));
     Assert.assertTrue(departLabel.isDisplayed());
     WebElement datePickerDepart =driver.findElement(By.cssSelector(".react-date-picker"));
     Assert.assertTrue(datePickerDepart.isDisplayed());
+
     WebElement returnLabel = driver.findElement(By.cssSelector("form>div>div:nth-child(6)>label"));
     Assert.assertTrue(returnLabel.isDisplayed());
     WebElement datePickerReturn =driver.findElement(By.cssSelector(".react-date-picker__inputGroup"));
     Assert.assertTrue(datePickerReturn.isDisplayed());
+
     WebElement numPassengerLabel = driver.findElement(By.cssSelector("form>div>div:nth-child(7)>label"));
     Assert.assertTrue(numPassengerLabel.isDisplayed());
     WebElement numPassengerBox =driver.findElement(By.cssSelector("form>div>div:nth-child(7)>div"));
     Assert.assertTrue(numPassengerBox.isDisplayed());
-    List<WebElement> option1 =driver.findElements(By.cssSelector("form>div>div:nth-child(7)>div>select>option"));
-    int [] numbers ={1,2,3,4,5,6,7,8,9};
-    for (int i = 0; i < numbers.length; i++) {
-    }
+    Assert.assertEquals(numPassengerLabel.getText(), "Number of passengers");
+    WebElement passengerNumbers =driver.findElement(By.cssSelector("form>div>div:nth-child(7)>div>select"));
+    Assert.assertEquals(passengerNumbers.getAttribute("value"),"1");
+
+
+
+
+    WebElement Passenger1Label = driver.findElement(By.cssSelector("form>div>div:nth-child(8)>label"));
+    Assert.assertTrue(Passenger1Label.isDisplayed());
+    Assert.assertEquals(Passenger1Label.getText(), "Passenger 1");
+    WebElement Passenger1Box =driver.findElement(By.cssSelector("form>div>div:nth-child(8)>div"));
+    Assert.assertTrue(Passenger1Box.isDisplayed());
+    WebElement PassengerType =driver.findElement(By.cssSelector("form>div>div:nth-child(8)>div>select"));
+    Assert.assertEquals(PassengerType.getAttribute("value"),"Adult (16-64)");
+
+    WebElement bookButton = driver.findElement(By.cssSelector("button[class^='Button']"));
+    Assert.assertTrue(bookButton.isDisplayed());
+    Assert.assertTrue(bookButton.isEnabled());
+
+
 
 }}
